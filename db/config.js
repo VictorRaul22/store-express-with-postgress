5c604d209d4ff3ace4b7d0d115d52b3e06378ebe
VictorRaul22 <victor-raul-22@hotmail.com>
2023-03-31 10:49:40 -0500

    user migracition

--- db/config.js
+++ db/config.js
@@ -1,6 +1,4 @@
-const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
-const setUpModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
@@ -8,10 +6,13 @@ const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI_TYPE = 'postgres'; //  mysql | postgres
 const URI = `${URI_TYPE}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres', //aqui 10:55
-  logging: console.log,
-});
-setUpModels(sequelize);
-// sequelize.sync({ logging: console.log });
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  },
+};
