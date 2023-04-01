d066ddeb046a54e3eb24dba6f9b1e6c6cd2fca75
VictorRaul22 <victor-raul-22@hotmail.com>
2023-04-01 01:51:00 -0500

    fix migrations

--- db/migrations/20230329062501-create-user.js
+++ db/migrations/20230329062501-create-user.js
@@ -1,14 +1,37 @@
 'use strict';
 
-const { UserSchema, USER_TABLE } = require('../models/user.model');
+const { DataTypes } = require('sequelize');
+const { USER_TABLE } = require('../models/user.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(USER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'create_at',
+        defaultValue: Sequelize.NOW,
+      },
+    });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.dropTable(USER_TABLE);
   },
 };
--- db/migrations/20230329065544-add-role-table-user.js
+++ db/migrations/20230329065544-add-role-table-user.js
@@ -1,13 +1,18 @@
 'use strict';
+const { DataTypes } = require('sequelize');
 const { UserSchema, USER_TABLE } = require('../models/user.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn(USER_TABLE, 'role', UserSchema.role);
+  async up(queryInterface) {
+    await queryInterface.addColumn(USER_TABLE, 'role', {
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: 'customer',
+    });
   },
 
-  async down(queryInterface, Sequelize) {
+  async down(queryInterface) {
     await queryInterface.removeColumn(USER_TABLE, 'role', UserSchema.role);
   },
 };
